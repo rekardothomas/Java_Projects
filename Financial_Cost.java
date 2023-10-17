f56ac1eba9d05eb36acb8a591b62a9e4f05d5319
Rekardo Thomas <88115126+rekardothomas@users.noreply.github.com>
2023-10-17 16:39:04 -0500

    Add files via upload

---
+++ Financial_Cost.java
@@ -0,0 +1,50 @@
+/**
+
+ Name: Rekardo Thomas
+ Course: CIS 144
+ Purpose:Chapter 3 - Coding assignments CIS 144
+ This displays code for programming exercises 3.1, 3.17, 3.25, 3.33
+
+ */
+
+
+import java.util.Scanner;
+
+public class Financial_Cost {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        //Enter weight of rice package 1
+        System.out.print("Enter weight in pounds for package 1: ");
+        double riceWeight1 = input.nextDouble();
+
+        //Enter price of rice package 1
+        System.out.print("Enter price for package 1: $");
+        double ricePrice1 = input.nextDouble();
+
+        //Enter weight of rice package 2
+        System.out.print("Enter weight in pounds for package 2: ");
+        double riceWeight2 = input.nextDouble();
+
+        //Enter price of rice package 2
+        System.out.print("Enter price for package 2: $");
+        double ricePrice2 = input.nextDouble();
+
+        double package1_total = ricePrice1 / riceWeight1;
+
+        double package2_total = ricePrice2 / riceWeight2;
+
+        if (package1_total > package2_total) {
+            System.out.println("Package 2 is a better deal.");
+        } else if (package1_total < package2_total) {
+            System.out.println("Package 1 is a better deal.");
+        } else if (package1_total == package2_total) {
+            System.out.println("Packages are the same.");
+
+        }
+
+
+    }
+}
